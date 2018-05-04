f24b6247c445147ebc564d43226d99d7a624f5dc
kbs1 <karl.schliep@nist.gov>
2018-05-04 14:14:38 -0400

    Uploading More Working Files from NIST:

---
+++ history.py
@@ -0,0 +1,520 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Mar  1 08:57:46 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/temp.py', wdir='C:/Users/kbs1/.spyder-py3')
+root.file
+runfile('C:/Users/kbs1/.spyder-py3/temp.py', wdir='C:/Users/kbs1/.spyder-py3')
+open(root.file)
+root.file
+root.file.name
+open(root.file.name)
+a=open(root.file.name)
+a
+a=open(root.file.name,'r')
+a
+b=open(root.file.name,'r')
+b
+path = root.file.name
+c=open(path,'r')
+c
+path
+c.read()
+a.read()
+c.readlines()
+b.readlines()
+d=a.readlines()
+d
+d=b.readlines()
+d
+b.readlines()
+c.readlines()
+runfile('C:/Users/kbs1/.spyder-py3/temp.py', wdir='C:/Users/kbs1/.spyder-py3')
+file_by_line
+file_by_line[1:3]
+file_by_line[0:3]
+file_by_line[0:4]
+file_by_line[0:5]
+file_by_line[0:10]
+runfile('C:/Users/kbs1/.spyder-py3/temp.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+file_by_line[0]
+file_by_line[1]
+file_by_line[0:1]
+file_by_line[0:10]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+Header
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+Header
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+document
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+my_gui
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+
+## ---(Mon Mar 12 11:42:26 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/temp.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+print(my_gui)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_by_line
+data_by_line[Header:-1]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+Header
+Header_size.get()
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+Header_size
+Header_size.get()
+Header_size.get(1)
+Header_size.get(0)
+Header_size.cget()
+Header_size.get
+Header_size.get()
+a = Header_size.get()
+a
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+a = Header_size.get()
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+v
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+v
+Header
+a=v.get()
+a
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+v
+Header
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+Header
+a=Header_size.get()
+Header_size
+Header_size.get()
+v
+v.get()
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+v.get()
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/untitled0.py', wdir='C:/Users/kbs1/.spyder-py3')
+a
+runfile('C:/Users/kbs1/.spyder-py3/untitled0.py', wdir='C:/Users/kbs1/.spyder-py3')
+a
+runfile('C:/Users/kbs1/.spyder-py3/untitled0.py', wdir='C:/Users/kbs1/.spyder-py3')
+a
+runfile('C:/Users/kbs1/.spyder-py3/untitled0.py', wdir='C:/Users/kbs1/.spyder-py3')
+a
+a*a
+runfile('C:/Users/kbs1/.spyder-py3/untitled0.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_1.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_2.py', wdir='C:/Users/kbs1/.spyder-py3')
+asdf
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_2.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_3.py', wdir='C:/Users/kbs1/.spyder-py3')
+
+## ---(Wed Mar 14 10:07:08 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_3.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_by_line
+data_by_line[0:2]
+data_by_line[1][1]
+size(data_by_line)
+a=data_by_line.split(' ')
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_3.py', wdir='C:/Users/kbs1/.spyder-py3')
+np.asarray(data_by_line)
+a=np.asarray(data_by_line)
+size(a)
+a.size
+a.length
+b = split_special(data_by_line, \n)
+b = split_special(data_by_line, '\n')
+data_by_line
+len(data_by_line)
+data_by_line.count('\n')
+data_by_line.count(\n)
+data_by_line.count(',')
+data_by_line.count('\n')
+data_by_line.count('x')
+data_by_line.count(0)
+data_by_line.count('0')
+data_by_line.split('\n')
+a=data_by_line[0:END).split('\n')
+a=data_by_line[0:END].split('\n')
+a=data_by_line[0:-1].split('\n')
+a=data_by_line[0].split('\n')
+a
+a.split(' ')
+a[0]
+a[1]
+data_by_line[0]
+data
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_3.py', wdir='C:/Users/kbs1/.spyder-py3')
+data
+a
+a=data_by_line[0].split(' ')
+a
+a.strip()
+a=data_by_line[0].strip()
+a
+a=data_by_line[0].delete(' ')
+a=data_by_line.delete(' ')
+a=data_by_line.replace(' ', '')
+a=data_by_line[0].replace(' ', '')
+a
+a=data_by_line[0].replace('\n', '')
+a
+a[0]
+a[1]
+a[2]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_delimited
+len(data_delimited)
+data_delimited.split(' ')
+data_delimited[0].split(' ')
+data_delimited[0].split('* ')
+data_delimited[0].split(r'* ')
+data_delimited[0].split()
+a=data_delimited[0].split()
+b=np.asarray(a)
+b
+b[1]
+b[0]
+b[3]
+b[2]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+for lines in data: print(lines)
+len(lines)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array
+data_array.size
+data_array.shape
+data_delimited
+len(data_delimited)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array.shape
+data_delimited
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+lines
+lines.rstrip()
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_delimited
+data_delimited.size()
+len(data_delimited)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_delimited[1]
+data_delimited[0]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+line
+lines
+lines.rstrip()
+data_delimited[1].split()
+b=data_delimited[1].split()
+b
+len(b)
+c=np.asarray(b)
+c
+c.shape
+data_delimited[a].split()
+data_delimited[0].split()
+data_delimited[1].split()
+data_delimited[0].split()
+data_delimited[2].split()
+len(data_delimited)
+data_array
+data_array.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+
+## ---(Mon Apr  2 10:56:53 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+import pandas as pd
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data[0,0]
+data.size
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array.shape
+data_delimited.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_4.py', wdir='C:/Users/kbs1/.spyder-py3')
+a=data_array.reshap(2)
+a=data_array.reshape(2)
+a=data_array.reshape(990/4,990/4)
+a=data_array.reshape(99,10)
+a
+t=np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
+t
+t.shape
+t.reshape(3,3)
+t.reshape(2,5)
+for i from i=1:10;print(i)
+for i in [1:10];print(i)
+for i in range(data_array.shape[0]):; print(i)
+for i in range(data_array.shape[0]): print(i)
+reordered_shape_columns = data_array.shape[0]/ Row_size
+math.ceil(495/16)
+ceil(495/16)
+np.ceil(495/16)
+np.ceil(496/16)
+np.ceil(498/16)
+np.ceil(499/16)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+b=int(np.ceil(data_array.shape[0]/ Row_size))
+b
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+for i in range(np.ceil(data_array.shape[0]/Row_size)):
+                print(i)
+                
+range(7)
+np.range(7)
+np.arange(7)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+np.arange(np.ceil(data_array.shape[0]/Row_size))
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+Row_size.dtype
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered
+data_array_reordered.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered
+data_array_reordered.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+[1:4]
+(1:4)
+np.arange(1:4)
+np.arange(1,4)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+Row
+Row_size
+30*16
+31*16
+np.arange(np.ceil(data_array.shape[0]/Row_size))
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+int(i*Row_size):it_row
+np.arange(int(i*Row_size):it_row)
+np.arange(int(i*Row_size),it_row)
+data_array.shape
+data_array_reordered.shape
+Row_size
+data_array[0:16,0:Column_size]
+b=data_array[0:16,0:Column_size]
+b.shape
+i=30
+it_row = int((i+1)*Row_size)
+                it_col = int((i+1)*Column_size)
+it_row = int((i+1)*Row_size; it_col = int((i+1)*Column_size)
+it_row = int((i+1)*Row_size
+Row_size
+it_row = int((i+1)*16
+it_row = int((30+1)*16
+it_row = int((i+1)*Row_size)
+it_col = int((i+1)*Column_size)
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+c.shape
+d=np.arange(int(i*Row_size),it_row)
+d.shape
+i*Row_size
+it_row
+i=0
+i*Row_size
+it_row
+it_row = int((i+1)*Row_size)
+it_row
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+it_row = int((i+1)*Row_size)
+it_col = int((i+1)*Column_size)
+c=data_array[int(i*Row_size):it_row-1,0:Column_size]
+c.shape
+i=30
+c=data_array[int(i*Row_size):it_row-1,0:Column_size]
+c.shape
+it_col = int((i+1)*Column_size)
+it_row = int((i+1)*Row_size)
+it_row
+c=data_array[int(i*Row_size):it_row-1,0:Column_size]
+c.shape
+i*Row_size
+it_row-1
+c=data_array[int(480:496-1,0:Column_size]
+c=data_array[int(480):496-1,0:Column_size]
+c.shape
+c=data_array[480:496-1,0:Column_size]
+c.shape
+c=data_array[0:16,0:Column_size]
+c.shape
+c=data_array[480,496:Column_size]
+c.shape
+c=data_array[480:496,0:Column_size]
+c.shape
+c=data_array[480:END,0:Column_size]
+c=[[1,2],[3,4],[5,6],[7,8],[9,10]]
+c=np.array(c)
+c.shape
+d=c[3:5,2]
+d=c[3:5,1]
+d.shape
+d=c[3:5,0:1]
+d.shape
+d
+d=c[3:5,0:2]
+d.shape
+Column_size
+d=c[0:2,0:2]
+d.shape
+d
+d[0,0]
+d[1,0]
+i=1
+it_col = int((i+1)*Column_size)
+it_row = int((i+1)*Row_size)
+c=data_array[int(i*Row_size):it_row-1,0:Column_size]
+c.shape
+c=data_array[i*Row_size:it_row-1,0:Column_size]
+c.shape
+c=data_array[i*Row_size:it_row,0:Column_size]
+c.shape
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+c.shape
+c=data_array_reordered[0:Row_size,int(i*Column_size):it_col]
+c.shape
+i=30
+it_row = int((i+1)*Row_size)
+it_col = int((i+1)*Column_size)
+c=data_array_reordered[0:Row_size,int(i*Column_size):it_col]
+c.shape
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+c.shape
+c=data_array[int(480):496,0:Column_size]
+data_array[end,end]
+data_array[End,END]
+data_array[END,END]
+data_array[-1,-1]
+iteration_length
+Column_size
+Row_size*30
+data_array.shape[0]-30*Row_size
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered[0:Row_size,i*Row_size:data_array.shape[0]]
+d=data_array_reordered[0:Row_size,i*Row_size:data_array.shape[0]]
+d.shape
+data_array.shape[0]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+last
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+last
+c=data_array_reordered[0:Row_size,i*Row_size:last]
+c.shape
+i=30
+i*Row_size
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+i
+it_row = int((i+1)*Row_size)
+i=0
+it_row = int((i+1)*Row_size)
+it_col = int((i+1)*Column_size)
+data_array[int(i*Row_size):it_row,0:Column_size]
+c=data_array_reordered[0:Row_size,i*Row_size:last]
+c.shape
+Column_size
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+c.shape
+data_array_reordered[0:last_size,int(i*Column_size):it_col]
+last_size= last-i*Row_size
+data_array_reordered[0:last_size,int(i*Column_size):it_col]
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+c=data_array_reordered[0:Row_size,i*Row_size:last]
+c.shap
+c.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+iteration_length
+i==iteration_length[-1]
+i=iteration_length[-1]
+last_size= last-i*Row_size
+it_row = int((i+1)*Row_size)
+it_col = int((i+1)*Column_size)
+data_array_reordered[0:last_size,int(i*Column_size):it_col]=data_array[int(i*Row_size):it_row,0:Column_size]
+data_array_reordered[0:last_size,int(i*Column_size):it_col]
+last_size= int(last-i*Row_size)
+data_array_reordered[0:last_size,int(i*Column_size):it_col]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered
+data_array_reordered.shape
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+Row_size
+it_row
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+i=0
+c=data_array[int(i*Row_size):it_row,0:Column_size]
+it_row = int((i+1)*Row_size)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered.shape
+data_array_reordered[20,50]
+data_array_reordered[19,50]
+data_array_reordered[19,49]
+data_array_reordered[19,48]
+data_array_reordered[19,47]
+data_array_reordered[18,48]
+data_array_reordered[17,48]
+data_array_reordered[16,48]
+data_array_reordered[:,48]
+path
+pathn=os.path.basename(path)
+pathn
+pathn=os.path.sep(path,object='.')
+pathn=os.path.sep(path,\'.')
+pathn=os.path.sep(path,'.')
+a=os.post.splitext(path)[0]
+a=os.path.splitext(path)[0]
+a
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_array_reordered[0,0]
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+data_by_line.size
+np.size(data_by_line)
+runfile('C:/Users/kbs1/.spyder-py3/Data_Reorganizer_5.py', wdir='C:/Users/kbs1/.spyder-py3')
+5/8
+runfile('C:/Users/kbs1/.spyder-py3/C0 Characterization.py', wdir='C:/Users/kbs1/.spyder-py3')
+t
+t=np.arange(0,20,1)
+t
+runfile('C:/Users/kbs1/.spyder-py3/C0 Characterization.py', wdir='C:/Users/kbs1/.spyder-py3')
+mA*t+bA
+mA
+mB
+runfile('C:/Users/kbs1/.spyder-py3/C0 Characterization.py', wdir='C:/Users/kbs1/.spyder-py3')
+dxA
+dyA
+C0A[1,1]
+2.54*27.625-2
+C0A[1,0]
+C0A[0,0]
+C0A[1,1]-C0A[0,1]
+C0A[1,1]
+-C0A[0,1]
+2.54*19.75-2
+mA
+20/.6
+.6/20
+runfile('C:/Users/kbs1/.spyder-py3/C0 Characterization.py', wdir='C:/Users/kbs1/.spyder-py3')
+os.path
+save_loc
+
+## ---(Mon Apr 23 15:22:38 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+
+## ---(Mon Apr 30 09:55:09 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+np.arange(1,28,2)
+a=np.arange(1,28,2)
+a
+[1,3,5,7,9]
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+np.arange(0,16,1)
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+
+## ---(Mon Apr 30 11:29:48 2018)---
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+output
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+file_list
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker.py', wdir='C:/Users/kbs1/.spyder-py3')
+runfile('C:/Users/kbs1/.spyder-py3/PDF_Worker_Rotater.py', wdir='C:/Users/kbs1/.spyder-py3')
